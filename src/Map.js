e8092c12dacaf6f09762945e4dfff0ec54c03370
DevonSmith91 <DevonSmith91@gmail.com>
2020-03-12 22:03:22 -0400

    state line drawn.

--- src/Map.js
+++ src/Map.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Map, TileLayer } from "react-leaflet";
+import { Map, TileLayer,  GeoJSON } from "react-leaflet";
 import "./map.css";
+import borderData from './Border.js'
 
 
 export default function Maps() {
+
+
+
     return (
-        <Map center={[44.5588, -72.5778]} zoom={8} >
+        <Map center={[43.8988, -72.5778]} zoom={8} zoomControl= {false}  >
             <TileLayer
                 url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}' 
                 attribution = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-            
             />
+            <GeoJSON key='my-geoJson' data = {borderData}/>
         </Map>
     );
 }
