0506f7eee39dd74e822782505fc8cc042b45d4d6
DevonSmith91 <DevonSmith91@gmail.com>
2020-03-12 22:23:57 -0400

    map is in a fixed location, First Story Finished

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, TileLayer,  GeoJSON } from "react-leaflet";
+import { Map, TileLayer, GeoJSON, } from "react-leaflet";
 import "./map.css";
 import borderData from './Border.js'
 
@@ -9,7 +9,7 @@ export default function Maps() {
 
 
     return (
-        <Map center={[43.8988, -72.5778]} zoom={8} zoomControl= {false}  >
+        <Map center={[43.8988, -72.5778]} zoom={8} zoomControl= {false} boxZoom={false} doubleClickZoom={false} dragging={false} keyboard={false} scrollWheelZoom={false} touchZoom={false}>
             <TileLayer
                 url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}' 
                 attribution = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
